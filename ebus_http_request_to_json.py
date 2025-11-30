bee9f9a96958a9e6103289c0c5b278ae98e6ac96
Maarten <72400239+mintgroen@users.noreply.github.com>
2025-11-30 21:25:52 +0100

    Delete ebus_http_request_to_json.py

--- ebus_http_request_to_json.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-#
-# This script is obsolete and has been removed.
-# Its functionality has been merged into 'json_schema_generator.py'.
-#
-print("This script is obsolete. Please use 'json_schema_generator.py'.")
